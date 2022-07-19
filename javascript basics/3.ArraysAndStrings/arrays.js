3acfca5b1dad34af54d6091cf69594a204bb659e
Emre Özdin <mrzdnboun@gmail.com>
2022-07-20 00:04:06 +0300

    arraysv2

--- javascript basics/3.ArraysAndStrings/arrays.js
+++ javascript basics/3.ArraysAndStrings/arrays.js
@@ -6,3 +6,21 @@ console.log('********* ARRAYS ************');
 
 //* Dizi Tanımlama
 //* ----------------------------------------------------------------------------------------------
+
+//! 1. Yöntem Array literal - tercih edilen yöntem
+const isimler = ["ahmet", "mehmet", "ismet", "saffet"];
+console.log(isimler);
+console.log(isimler.length); //4 
+
+//! 2. Yöntem (Object Constructor)
+const diller = new Array ("Python", "Java", "C++", "JS", "Go");
+console.log(diller);
+
+const numbers = new Array (3,2,1);
+console.log(numbers);
+
+const sayilar = new Array (10); // 10 elemanlık boş bir array oluşturdu
+console.log(sayilar);
+
+
+
