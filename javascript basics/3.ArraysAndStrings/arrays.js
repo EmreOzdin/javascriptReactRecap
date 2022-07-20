90391599ca72ed1778cd46ed4c6496e628e26654
EmreOzdin <mrzdnboun@gmail.com>
2022-07-20 15:01:32 +0300

    arrays son dosya

--- javascript basics/3.ArraysAndStrings/arrays.js
+++ javascript basics/3.ArraysAndStrings/arrays.js
@@ -49,5 +49,38 @@ console.log(sonYas, kisiler);
 
 const cars = ["BMW", "Porsche", "Mercedes", "Fiat"];
 
+//* pop() son elemanı siler ve sildiği elemanı döndürür
 const deleted = cars.pop();
 console.log(cars, "Silinen", deleted);
+
+//* push() son elemanı ekler
+const n = cars.push("volvo");
+console.log(cars, n);
+
+//* unshift() ilk elemanı siler ve sildiği elemanı döndürür
+const boyut = cars.unshift("Audi");
+console.log(boyut, cars);
+
+//* shift() ilk elemanı siler ve sildiği elemanı döndürür
+const silinen = cars.shift();
+console.log(silinen);
+
+//* reverse() diziyi tersine çevirir
+console.log(cars.reverse());
+console.log(cars);
+
+//* sort() diziyi alfabetik olarak sıralar
+console.log(cars.sort());
+
+const rakamlar = [3, 5, 1, 10, 55];
+console.log(rakamlar.sort()); //! alfabetik sıraladğı için doğru sonuç çıkmayabilir.
+
+//* sort metodu diziyi iterasyona uğratır ve parametre olarak aldığı arrow fonksiyonunu dizinin her bir elemanına uygular. Bu sayede küçük sayılar ile büyük sayıları yer değiştirerek sıralama işlemini gerçekleştirir. 
+rakamlar.sort((a,b) => a - b);
+console.log(rakamlar);
+
+cars.splice(1, 0, "Vosvos");
+console.log(cars); // 1. indise Vosvos ekle(eleman sayısı bir artar)
+
+cars.splice(2, 1, "Anadol");
+console.log(cars); // 2. indistekini Anadol olarak değiştir (Eleman sayısı değişmez)
