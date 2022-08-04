5be8908d0980287cf1d55648207bd1326fdf4de4
Emre Özdin <mrzdnboun@gmail.com>
2022-08-04 11:31:44 +0300

    krediHesaplamaSon

--- javascript advanced/js10_dom-examples/kredi-hesaplama/1-app.js
+++ javascript advanced/js10_dom-examples/kredi-hesaplama/1-app.js
@@ -26,4 +26,34 @@ hesaplaBtn.addEventListener("click", (e) => {
     ((1+faiz) ** vade.value - 1);
 
     //console.log(taksit);
+    sonuclariGoster();
 });
+const sonuclariGoster = () => {
+    const sonuclar = document.querySelector(".sonuclar");
+    sonuclar.innerHTML = ` <table class="table table-bordered border-warning mt-4">
+    <tbody>
+        <tr>
+        <th>Kredi Miktari</th>
+        <td>${tutar.value}₺</td>
+        <th>Kredi Tipi</th>
+        <td>${select.value}</td>
+        </tr>
+        <tr>
+        <th>Vade</th>
+        <td>${vade.value}</td>
+        <th>Faiz Orani</th>
+        <td>${oran}</td>
+        </tr>
+        <tr>
+        <th>Toplam Tutar</th>
+        <td>${(taksit*vade.value).toFixed()}₺</td>
+        <th>Taksit Tutari</th>
+        <td>${taksit.toFixed()}₺</td>
+        </tr>
+    </tbody>
+    </table>
+        </table>
+    
+    
+    `;
+};
