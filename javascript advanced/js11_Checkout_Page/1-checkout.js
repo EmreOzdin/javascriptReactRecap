679e80e394be3d94bfe50c0fa3b8624ce4559504
Emre Özdin <mrzdnboun@gmail.com>
2022-08-07 20:11:55 +0300

    capturing method aaded

--- javascript advanced/js11_Checkout_Page/1-checkout.js
+++ javascript advanced/js11_Checkout_Page/1-checkout.js
@@ -2,6 +2,14 @@ const taxRate = 0.18;
 const shippingPrice = 15.0;
 
 window.addEventListener("load", () => {
+    //setItem to localStorage
     localStorage.setItem("taxRate", taxRate);
     localStorage.setItem("shippingPrice", shippingPrice);
+
+    //setItem to sessionStorage
+    // sessionStorage.setItem("taxRate", taxRate);
+    // sessionStorage.setItem("shippingPrice", shippingPrice);
 });
+
+//capturing method 
+
