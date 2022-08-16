faf06cc0becb4e0fe49e95d4d2b445d396fe4b61
Emre Özdin <mrzdnboun@gmail.com>
2022-08-17 01:43:13 +0300

    footer and header added

--- React/rc02_jsx_basicStyling/src/App.js
+++ React/rc02_jsx_basicStyling/src/App.js
@@ -1,32 +1,31 @@
-//? React,JSX kullanmaktadir.
-//? JSX'de, HTMl elementleri dogrudan JS icerisinde kullanilabilir
+//? App componenti index.js dosyasinda cagrilir.
+//? React, JSX kullanmaktadır.
+//? JSX'de, HTML elementleri doğrudan JS içerisinde kullanılabilir.
 
-import Content from './Content';
-import Footer from './Footer';
-import Header from './Header';
+//! Componentler, HTML, CSS ve JS kodlarından meydana gelmiş
+//! bir kod parçasıdır.
 
-//! Componentler, HTML,CSS ve JS kodlarindan meydana gelmiş
-//! bir kod parcacigidir.
+//! Bir componentin return () kısmı ise JSX kodları içerir.
 
-//! Bir componentin return () kismi ise JSX kodlari icerir.
+//! JSX'in kendine göre bazi kuralları vardır. Örneğin bir
+//! component eger birden fazla HTML elementi dondurmesi 
+//! gerekirse bunlari tek bir container elemanı ile 
+//! sarmallayarak döndürmelidir. container için div, article,
+//! header, <> vb. herhangi bir element kullanılabilir.
 
-//! JSX'in kendine gore bazi kurallari vardir. Ornegin bir
-//! component eger birden fazla html elementi dondurmesi
-//! gerekirse bunlari tek bir container elemani ile sarmayalarak
-//! dondurmelidir.container icin div, article, header, <> v.b
-//! herhangi bir element kullanilabilir.
+//? App componenti tanımlanması
 
-//? App componentinin tanimlanmasi
+import Footer from './Footer.jsx';
+import Header from './Header.jsx';
+
 function App() {
-  //* JSX
-  //? stillendirme yapilmayacaksa container icin <> kullanilabilir
   return (
     <>
       <Header />
-      <Content />
       <Footer />
     </>
   );
-}
+};
+
 
 export default App;
--- React/rc02_jsx_basicStyling/src/Footer.jsx
+++ React/rc02_jsx_basicStyling/src/Footer.jsx
@@ -1,10 +1,10 @@
 
-//? Componentler fonksiyonlarda oldugu gibi 3 farkli yontemle yazilabilir.
-//? JSX icerisinde degiskenler {} icerisinde gosterilir.
+//? Footer componenti
 
-//? Footer comp.
 const Footer = () => {
-  return <footer>Copyright by Clarusway, {new Date().getFullYear()} </footer>;
-};
+    return (
+        <footer>Copyright by Clarusway, {new Date().getFullYear()}</footer>
+    )
+}
 
 export default Footer;
