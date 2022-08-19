34ef9f2def3659b910e37bbed96ecf33cab597f8
Emre Özdin <mrzdnboun@gmail.com>
2022-08-19 10:39:57 +0300

    module.css added

--- React/rc03_module-css/src/App.js
+++ React/rc03_module-css/src/App.js
@@ -1,19 +1,17 @@
-import Card from "./components/card/Card"; 
-import Button from "./components/button/Button";
-import data from "./util/data";
+import Card from './components/card/Card';
+import data from './util/data';
 
-function App () {
+function App() {
   console.log(data);
   return (
     <>
-    {data.map((item) => {
-      //! Destructring
-      const { id, img, btnName, dil } = item;
-      return <Card key= {id} img={img} btnName={btnName} dil={dil}/>
-
+      {data.map((item) => {
+        //!Destr
+        const { id, img, btnName, dil } = item;
+        return <Card key={id} img={img} btnName={btnName} dil={dil} />;
       })}
     </>
-  )
+  );
 }
 
 export default App;
