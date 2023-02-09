142d7bb063a9144bd9991f6e17758a6e123f1e24
Emre Özdin <mrzdnboun@gmail.com>
2023-02-09 16:08:26 +0300

    scss added

--- React/rc03_sass/src/App.js
+++ React/rc03_sass/src/App.js
@@ -2,8 +2,6 @@ import Card from './components/Card';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
-import './scss/App.scss';
-import data from './data';
 
 function App() {
   return (
